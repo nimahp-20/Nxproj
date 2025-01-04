32ae86de93e91ec089cf24a39da0473923c6b5bd
unknown <nimaneon17@gmail.com>
2025-01-04 22:21:15 +0330

    searcBox added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,17 @@
+import SearchForm from "../components/SearchForm";
+
 export default function Home() {
   return (
     <>
-    home
+      <section className="pink_container">
+        <div className="heading w-full">
+          Pitch your startUp, <br /> Connect with entrepreneurs
+        </div>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas,Vote on Pitches, and Get Noticed in virtual Competitions
+        </p>
+        <SearchForm/>
+      </section>
     </>
   );
 }
